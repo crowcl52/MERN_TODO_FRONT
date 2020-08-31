05990cecdd3115fd547e532acac859276278cc09
Eduardo Sanchez <esanchez@beesoftware.mx>
2020-08-30 21:15:26 -0500

    change port

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clientAxios =  axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL
+    baseURL: 'https://merntodo-serve.herokuapp.com'
 });
 
 export default clientAxios;
